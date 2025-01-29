e3ae4335530699377b22b1f76f2f78a75cc673d1
Kendall Fretwell <kfrtwl@gmail.com>
2025-01-29 18:01:17 -0500

    fixed smiley to  display properly

--- funky.theme.sh
+++ funky.theme.sh
@@ -7,15 +7,16 @@
 
 bash_op='\[\033[34m\][\[\033[0m\]'
 blue_cp='\[\033[34m\]]\[\033[0m\]'
-green='\[\033[32m\]'
-red='\[\033[31m\]'
+green='\033[0;32m'
+red='\033[0;31m'
 
 path_p="${blue_op}\w${blue_cp}"
 user_host="${blue_op}\u@\h${blue_cp}"
 ret_status="${blue_op}\$?${blue_cp}"
 hist_no="${blue_op}\!${blue_cp}"
 
-smiley='$(if [[    $? -eq 0 ]]; then echo -e "${green}:)";    else echo -e "${red}:("; fi)'
+
+ smiley='$(if [[    $? -eq 1 ]]; then echo -e "${green}:)";    else echo -e "${red}:("; fi)'
 
 PS1="╭─${path_p}─${user_host}─${ret_status}─${hist_no}
 ╰─${blue_op}${smiley}${blue_cp} $ "
