e0831c08636e8b4ec30d22dbd5deee16dacc969e
Kendall Fretwell <kfrtwl@gmail.com>
2025-01-30 04:45:53 -0500

    finalized-code

--- funky.theme.sh
+++ funky.theme.sh
@@ -5,7 +5,7 @@
 
 # translated from zsh to bash
 
-bash_op='\[\033[34m\][\[\033[0m\]'
+blue_op='\[\033[34m\][\[\033[0m\]'
 blue_cp='\[\033[34m\]]\[\033[0m\]'
 green='\033[0;32m'
 red='\033[0;31m'
@@ -16,9 +16,9 @@ ret_status="${blue_op}\$?${blue_cp}"
 hist_no="${blue_op}\#${blue_cp}"
 
 
- smiley='$(if [[    $? -eq 1 ]]; then echo -e "${green}:)";    else echo -e "${red}:("; fi)'
+ smiley="$(if [[    $? -eq 1 ]]; then echo -e "${green}:)";    else echo -e "${red}:("; fi)"
 
 PS1="╭─${path_p}─${user_host}─${ret_status}─${hist_no}
 ╰─${blue_op}${smiley}${blue_cp} $ "
 
-PS2="${blueop}${blue_cp}> "
+PS2="${blue_op}${blue_cp}> "
